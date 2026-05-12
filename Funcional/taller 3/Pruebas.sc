b1e239374675f47676310e8444cd4e07768cc421
juanjose-atuesta <juanjoseatuestaflor@gmail.com>
2026-05-12 09:30:14 -0500

    agregamos los otros ejercicios y sus casos de pruebas

---
+++ Funcional/taller 3/Pruebas.sc
@@ -0,0 +1,25 @@
+
+//casos de prueba ejercicio 1:
+
+ mostrar(Numero(4.0))
+ mostrar(Atomo('x'))
+ mostrar(Suma(Atomo('x'), Numero(2.0)))
+ mostrar(Expo(Atomo('x'), Numero(3.0)))
+ mostrar(Logaritmo(Atomo('x')))
+ mostrar(Suma(Suma(Atomo('x'), Numero(1.0)), Numero(2.0)))
+
+
+//casos de prueba ejercicio 3:
+
+ mostrar(Numero(9.0))
+ evaluar(Numero(9.0), Atomo('x'), 99.0)
+ mostrar(Atomo('x'))
+ evaluar(Atomo('x'), Atomo('x'), 7.0)
+ mostrar(Suma(Atomo('x'), Numero(2.0)))
+ evaluar(Suma(Atomo('x'), Numero(2.0)), Atomo('x'), 3.0)
+ mostrar(Prod(Atomo('x'), Atomo('x')))
+ evaluar(Prod(Atomo('x'), Atomo('x')), Atomo('x'), 4.0)
+ mostrar(Expo(Atomo('x'), Numero(3.0)))
+ evaluar(Expo(Atomo('x'), Numero(3.0)), Atomo('x'), 3.0)
+ mostrar(Logaritmo(Suma(Atomo('x'), Numero(2.0))))
+ evaluar(Logaritmo(Suma(Atomo('x'), Numero(2.0))), Atomo('x'), 5.0)
--- Funcional/taller 3/package.scala
+++ Funcional/taller 3/package.scala
@@ -9,16 +9,18 @@ package objetc Newton{
   case class Div ( e1 : Expr , e2 : Expr ) extends Expr
   case class Expo ( e1 : Expr , e2 : Expr ) extends Expr
   case class Logaritmo ( e1 : Expr ) extends Expr
-def mostrar(e: Expr): String = e match {
-  case Numero(n)      => if (n == n.toInt) n.toInt.toString else n.toString
-  case Atomo(a)       => "" + a + ""
-  case Suma(e1, e2)   => "(" + mostrar(e1) + " + " + mostrar(e2) + ")"
-  case Resta(e1, e2)  => "(" + mostrar(e1) + " - " + mostrar(e2) + ")"
-  case Prod(e1, e2)   => "(" + mostrar(e1) + " * " + mostrar(e2) + ")"
-  case Div(e1, e2)    => "(" + mostrar(e1) + " / " + mostrar(e2) + ")"
-  case Expo(e1, e2)   => "(" + mostrar(e1) + " ^ " + mostrar(e2) + ")"
-  case Logaritmo(e1)  => "(lg(" + mostrar(e1) + "))"
+
+  def mostrar(e: Expr): String = e match {
+  case Numero(d)       => d.toString
+  case Atomo(x)        => x.toString
+  case Suma(e1, e2)    => "(" + mostrar(e1) + " + " + mostrar(e2) + ")"
+  case Resta(e1, e2)   => "(" + mostrar(e1) + " - " + mostrar(e2) + ")"
+  case Prod(e1, e2)    => "(" + mostrar(e1) + " * " + mostrar(e2) + ")"
+  case Div(e1, e2)     => "(" + mostrar(e1) + " / " + mostrar(e2) + ")"
+  case Expo(e1, e2)    => "(" + mostrar(e1) + " ^ " + mostrar(e2) + ")"
+  case Logaritmo(e1)   => "(lg(" + mostrar(e1) + "))"
 }
+  
   def derivar(e:Expr, a:Atomo):Expr = {
     
     def derivarAux(e:Expr): Expr = e  match {
@@ -59,10 +61,41 @@ def mostrar(e: Expr): String = e match {
         case (e1,e2) => Expo(e1,e2) 
 
       }
-    case Resta(e1,e2) => (limpiar(e1),limpiar(e2)) match{
-      case (Numero(0.0), e) =>
-    }
+    case Resta(e1, e2) => (limpiar(e1), limpiar(e2)) match {
+      case (e1, Numero(0.0)) => e1       // x - 0 = x
+      case (e1, e2)          => Resta(e1, e2)
+}
+    case Numero(e) => Numero(e)
+    case Atomo(x) => Atomo(x)
 
 
   }
+
+  def evaluar(f: Expr, a: Atomo, v: Double): Double = f match {
+
+  case Numero(d)    => d
+
+  case Atomo(x)     =>
+    if (x == a.x) v else 0.0  // otros átomos no deberían aparecer según el enunciado
+
+  case Suma(e1, e2)    => evaluar(e1, a, v) + evaluar(e2, a, v)
+  case Resta(e1, e2)   => evaluar(e1, a, v) - evaluar(e2, a, v)
+  case Prod(e1, e2)    => evaluar(e1, a, v) * evaluar(e2, a, v)
+  case Div(e1, e2)     => evaluar(e1, a, v) / evaluar(e2, a, v)
+  case Expo(e1, e2)    => Math.pow(evaluar(e1, a, v), evaluar(e2, a, v))
+  case Logaritmo(e1)   => Math.log(evaluar(e1, a, v))
+
+}
+
+def raizNewton(f: Expr, a: Atomo, x0: Double,
+               ba: (Expr, Atomo, Double) => Boolean): Double = {
+  if (ba(f, a, x0))
+    x0
+  else {
+    val fx  = evaluar(f, a, x0)                        
+    val fpx = evaluar(derivar(f, a), a, x0)            
+    val x1  = x0 - (fx / fpx)                          
+    raizNewton(f, a, x1, ba)                           
+  }
+}
 }
